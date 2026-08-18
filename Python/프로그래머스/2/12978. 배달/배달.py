d4066118cce78c231f01b9e5fa866885f7e7ed78
Hyunsoo <irver456@naver.com>
2026-08-18 11:58:53 +0900

    [level 2] Title: 배달, Time: 1.04 ms, Memory: 11.8 MB -BaekjoonHub

--- Python/프로그래머스/2/12978. 배달/배달.py
+++ Python/프로그래머스/2/12978. 배달/배달.py
@@ -1,44 +1,38 @@
 import heapq
+from collections import defaultdict
 
 def solution(N, road, K):
     ans = 0
     dist = [float('inf')] * (N+1)
-    graph = {i+1: {} for i in range(N)}
+    graph = defaultdict(list)
     
     for v1, v2, c in road:
-        if graph[v1].get(v2, 0) != 0:
-            if graph[v1][v2] > c:
-                graph[v1][v2] = c
-                graph[v2][v1] = c
-        else:
-            graph[v1][v2] = c
-            graph[v2][v1] = c
+        graph[v1].append([v2, c])
+        graph[v2].append([v1, c])
     
     # print('graph', graph)
     
     def dijkstra(start, graph, dist):
-        heap = []
+        heap = [(0, start)]
         dist[start] = 0
-        for k, v in graph[start].items():
-            heapq.heappush(heap, (v, k))
         # print(heap)
         while heap:
             cur_dist, cur_v = heapq.heappop(heap)
             
             if (cur_dist > dist[cur_v]):
                 continue
-            dist[cur_v] = cur_dist
+            # dist[cur_v] = cur_dist
             
-            for next_v, d in graph[cur_v].items():
+            for next_v, d in graph[cur_v]:
                 next_dist = cur_dist + d
-                if (next_dist <= dist[next_v]):
-                    # dist[next_v] = next_dist
+                if (next_dist < dist[next_v]):
+                    dist[next_v] = next_dist
                     heapq.heappush(heap, (next_dist, next_v))
         
         return dist
     
     
-    dist = dijkstra(1, graph, dist)
+    dijkstra(1, graph, dist)
     # print(dist)
     for d in dist[1:]:
         if d <= K:
